4dd35c486d1f8a32b3776bcecf2244358eb4812c
TheMayfly <themayfly@gmail.com>
2020-07-19 19:09:59 -0500

    number of island

--- ALGO/numberOfIsland-200.js
+++ ALGO/numberOfIsland-200.js
@@ -2,41 +2,35 @@
  * @param {character[][]} grid
  * @return {number}
  */
-var Node = function([x, y], val) {
-  this.xy = xy;
-  this.val = val;
-};
-
-var getNeighbors = function([x, y], grid) {
-  let yLen = grid.length;
-  let xLen = grid[0].length;
-  [x, y] = [x*1, y*1];
-  let left = x-1 >= 0 ? grid[y][x-1] : null;
-  let right = x+1 < xLen ? grid[y][x+1] : null;
-  let top = y-1 ? >= 0 ? grid[y-1][x] : null;
-  let bottom = y+1 < yLen ? grid[y+1][x] : null;
-  return [left, right, top, bottom];
-};
-
-var isOutside = function(node, grid) {
-  let yLen = grid.length;
-  let xLen = grid[0].length;
-}
-
 var numIslands = function(grid) {
-  let map = {};
-  let islandCount = 0;
-  function traverse(node) {
-    if (!node || isOutside(node)) {
-      return;
-    } 
-    islandCount++;
-    node.val = '0'; // visited 
-    let [left, right, top, bottom] = getNeighbors(node);
-    traverse(left);
-    traverse(right);
-    traverse(top);
-    traverse(bottom);
+  if (!grid || grid.length === 0) {
+    return 0;
   }
-  
+  const rowLen = grid.length;
+  const colLen = grid[0].length;
+  let islandCount = 0;
+
+  function traverse(row, col) {
+    if (row >= rowLen || row < 0 || col >= colLen || col < 0) { // out of boundary
+      return;
+    }
+    if (grid[row][col] === '0') { // already visited or water
+      return;
+    }
+    grid[row][col] = '0';
+    traverse(row+1, col); //bottom
+    traverse(row-1, col); // top
+    traverse(row, col+1); // right
+    traverse(row, col-1); // left
+  }
+
+  for (let r = 0; r < rowLen; r++) {
+    for (let c = 0; c < colLen; c++) {
+      if (grid[r][c] === '1') {
+        islandCount++;
+        traverse(r, c);
+      }
+    }
+  }
+  return islandCount;
 };
